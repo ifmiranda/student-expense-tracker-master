dfbd42b8c58babae4f454d0fb00fa6c414ebc36d
Isabella Miranda <ifmiranda@usf.edu>
2025-12-05 17:12:52 -0500

    Added Expenses Chart

---
+++ assets/components/ExpensesChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
+
+export default function ExpensesChart({ labels, values }) {
+  return (
+    <View style={{ padding: 16 }}>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 18,
+          fontWeight: 'bold',
+          marginBottom: 8,
+        }}
+      >
+        Spending by Category
+      </Text>
+
+      <BarChart
+        data={{
+          labels: labels,
+          datasets: [{ data: values }],
+        }}
+        width={350}
+        height={220}
+        fromZero
+        chartConfig={{
+          backgroundGradientFrom: '#ffffff',
+          backgroundGradientTo: '#ffffff',
+          color: () => 'black',
+          labelColor: () => 'black',
+        }}
+        style={{ borderRadius: 12 }}
+      />
+    </View>
+  );
+}
